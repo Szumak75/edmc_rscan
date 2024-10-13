504d23a28483735bc3818579a9b31f93c1b82c77
Jacek Kotlarski <szumak@virthost.pl>
2024-10-13 02:06:19 +0200

    some fix

--- rscan/dialogs.py
+++ rscan/dialogs.py
@@ -584,8 +584,11 @@ class EdrsScanDialog(tk.Toplevel, TkBase, BLogClient, _BEdrsDialog):
 
         # update located system
         for item in self._stars:
+            self.logger.debug = (
+                f"ITEM: {item[0].name}, SS: {self._r_data.stars_system.name}"
+            )
             if item[0].name == self._r_data.stars_system.name:
-                item[2][_FontKeys.FONT] = self.__fonts._get_data(key=_FontKeys.FONT_STRIKE)  # type: ignore
+                item[2][_FontKeys.FONT] = self._fonts._get_data(key=_FontKeys.FONT_STRIKE)  # type: ignore
 
     def debug(self, currentframe: Optional[FrameType], message: str = "") -> None:
         """Build debug message."""
